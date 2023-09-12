7fbb011443dc3a884975f18664c75961c9e8b4c3
PFonsecaFV <pfonsecafv@gmail.com>
2023-09-12 15:30:37 -0300

    🎉 Iniciando api-planos - Configurando ambiente e banco de dados

---
+++ src/main/java/com/api/apiplano/ApiPlanoApplication.java
@@ -0,0 +1,14 @@
+package com.api.apiplano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlanoApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/api/apiplano/ApiPlanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apiplano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
