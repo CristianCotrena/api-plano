e3f0f4cb00f9bba2e9c9d5e2ca952cda80d709f3
PFonsecaFV <pfonsecafv@gmail.com>
2023-09-12 16:23:44 -0300

    🎨 Improve structure / format of the code.

--- src/main/java/com/api/plano/ApiPlanoApplication.java
+++ src/main/java/com/api/plano/ApiPlanoApplication.java
@@ -1,4 +1,4 @@
-package com.api.apiplano;
+package com.api.plano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/api/plano/ApiPlanoApplicationTests.java
+++ src/test/java/com/api/plano/ApiPlanoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.api.apiplano;
+package com.api.plano;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
