717e7c524e1788548b3a145f65384d68ee40fb7e
CristianCotrena <cristian.cotrena@gmail.com>
2023-09-13 11:13:52 -0300

    ♻️ Refatorando - Iniciando api-plano - Configurando ambiente e movendo para branch (develop)

---
+++ src/test/java/com/api/plano/ApiPlanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.plano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
